a021a8fc74af19aac84d738c47449cef5e0a45f4
许金泰 <1791639435@qq.com>
2026-07-07 16:49:11 +0800

    fix: 修复单元测试

--- packages/zoo/src/lib/zoo.spec.ts
+++ packages/zoo/src/lib/zoo.spec.ts
@@ -1,7 +1,12 @@
+import { vi } from 'vitest';
 import { zoo } from './zoo.js';
+
+vi.mock('@org/animal', () => ({
+  getRandomAnimal: () => ({ name: 'dog', sound: 'woof' }),
+}));
 
 describe('zoo', () => {
   it('should work', () => {
-    expect(zoo()).toEqual('zoo');
+    expect(zoo()).toEqual('[ZOO] dog says woof!');
   });
 });
