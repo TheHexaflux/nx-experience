14fe9a87fe69d3798121756bd4b8a4d4a3da3a06
许金泰 <1791639435@qq.com>
2026-07-07 17:41:01 +0800

    test: 测试调整代码

--- packages/zoo/src/lib/zoo.ts
+++ packages/zoo/src/lib/zoo.ts
@@ -1,8 +1,6 @@
 import { getRandomAnimal } from '@org/animal';
 import { formatMessage } from '@org/util';
 
-
-
 export function zoo(): string {
   const result = getRandomAnimal();
   const message = `${result.name} says ${result.sound}!`;
